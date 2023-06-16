db9c6a40a7815838fee8f7c3c79df2cad9153d8f
Francis_ldn <fnhh04@gmail.com>
2023-06-16 21:39:30 +0100

    Initial Commit

---
+++ CoffeeMaster/CoffeeMasterApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeMasterApp.swift
+//  CoffeeMaster
+//
+//  Created by Francis Hui on 16/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeMasterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
